0810cab16283b4bc489f9c2e9795bd362c5a0246
Lena Soroka <4ta.lenasoroka.1989@gmail.com>
2025-01-23 13:00:00 +0200

    Initial Commit

---
+++ ExchangeMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeMate
+//
+//  Created by Lena Soroka on 23.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
